5db00c84bc2f6b9340172a84d325e1f26d8a3296
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2023-10-15 10:30:50 +0200

    added name and email to sidahmedfaisal branch

--- mine.py
+++ mine.py
@@ -1,6 +1,6 @@
 # Define variables
-name = "PUT YOUR NAME"
-email = 'PUT YOUR EMAIL'
+name = "Sidahmed Faisal"
+email = 'sidahmedfaisal.sf@gmail.com'
 
 # Print the name and email address
 print("Name:", name)
